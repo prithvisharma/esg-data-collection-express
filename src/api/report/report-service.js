9fd7658f1409cb6d9efcb72eba1d699bbbc59b2f
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2023-04-01 23:23:29 +0530

    add(service): report service skeleton

---
+++ src/api/report/report-service.js
@@ -0,0 +1,11 @@
+const postReport = (req, res) => {}
+const getReportById = (req, res) => {}
+const getReportIdsByUsername = (req, res) => {}
+const getReportQuestions = (req, res) => {}
+
+module.exports = {
+    postReport,
+    getReportById,
+    getReportIdsByUsername,
+    getReportQuestions,
+}
