603474d2a340f814641a0e1553ab5457e302ec1e
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2023-04-03 01:36:15 +0530

    update(register-router): reportRouter

--- src/register-router.js
+++ src/register-router.js
@@ -1,7 +1,8 @@
 const { loginRouter } = require("./api/login/login-router")
+const { reportRouter } = require("./api/report/report-router")
 const { userRouter } = require("./api/user/user-router")
 
-const routers = [loginRouter, userRouter]
+const routers = [loginRouter, userRouter, reportRouter]
 
 function registerRouters(app) {
     routers.forEach((router) => app.use("/esg-collect", router))
